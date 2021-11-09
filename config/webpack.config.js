36acfae2ed9ba11215941dffb538f22e6286206d
John Noel <chaostangent@gmail.com>
2021-11-09 20:56:22 +0000

    Basic setup

---
+++ config/webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, 'index.tsx'),
+        head: path.resolve(__dirname, 'head.ts'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                loader: 'ts-loader',
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: () => ([
+                                    require('autoprefixer'),
+                                ]),
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                loader: 'source-map-loader',
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
+        alias: {
+            react: 'preact/compat',
+            'react-dom': 'preact/compat',
+        }
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[id].[contenthash].js',
+        path: path.resolve(__dirname, '../public/build/'),
+        publicPath: '/build/',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].[contenthash].css',
+            ignoreOrder: false,
+        }),
+        new WebpackManifestPlugin(),
+    ],
+};
